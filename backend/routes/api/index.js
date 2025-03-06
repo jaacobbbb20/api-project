422ac2a956e3dee11fd705146f14a42708f694f0
jaacobbbb20 <jhathaway2002@icloud.com>
2025-03-05 18:54:18 -0700

    Was messing with routes folder

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,7 +5,4 @@ router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
-
-
-
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,12 +4,14 @@ const apiRouter = require('./api');
 
 //router.get('/hello/world', function(req, res) {
 //  res.cookie('XSRF-TOKEN', req.csrfToken());
-//  res.send('Hello World!');
+// res.send('Hello World!');
 //});
 
 // backend/routes/index.js
 // ...
 // Add a XSRF-TOKEN cookie
+
+
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
