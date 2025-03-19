408824a993aa9ae58c9c2016e3d1f81da5cf757a
jaacobbbb20 <jhathaway.1@icloud.com>
2025-03-19 11:18:39 -0700

    Added Create a Spot Route

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Spot.init({
-    ownerId: { // Use "ownerId" instead of "ownerID" for consistency
+    ownerId: {
       type: DataTypes.INTEGER,
       allowNull: false,
     },
