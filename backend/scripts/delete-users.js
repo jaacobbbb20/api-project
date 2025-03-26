6863dfbe1dc868c9b3739524b37d1b90f706f859
Jacob Hathaway <124851843+jaacobbbb20@users.noreply.github.com>
2025-03-26 10:43:29 -0700

    Added comments to the delete user script

--- backend/scripts/delete-users.js
+++ backend/scripts/delete-users.js
@@ -1,10 +1,11 @@
 // Delete all or a certain user
     // TO USE: uncomment which await User.destroy line applies to you
+    // TO RUN: inside of backend thru the terminal, run 'node scripts/delete-users.js'
 const { User } = require('../db/models');
 
 (async () => {
-  //await User.destroy({ where: {} }); // remove all users
-  // await User.destroy({ where: { id: 4 } }); // remove id=4 only
+  // await User.destroy({ where: {} }); // remove all users
+  // await User.destroy({ where: { id: x } }); // remove a user by specific id
   console.log('✅ Users deleted');
   process.exit();
 })();
