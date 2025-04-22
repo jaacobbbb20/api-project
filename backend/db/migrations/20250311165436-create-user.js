c5c6486756c102569ba42150a98d4db29256c2b0
jaacobbbb20 <jhathaway.1@icloud.com>
2025-04-21 18:01:17 -0700

    modified user migration

--- backend/db/migrations/20250311165436-create-user.js
+++ backend/db/migrations/20250311165436-create-user.js
@@ -47,7 +47,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    }, { schema: 'housingSite' });
+    }, { schema: 'housing_site' });
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Users";
