15ed6333b7a2283a7ec6e5ad3482010d4eac8b01
jaacobbbb20 <jhathaway.1@icloud.com>
2025-04-21 19:19:43 -0700

    Messed with seeders again

--- backend/db/seeders/20250325040718-demo-spots.js
+++ backend/db/seeders/20250325040718-demo-spots.js
@@ -6,6 +6,7 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
+options.tableName = 'Spots';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
