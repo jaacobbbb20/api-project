a587f6a5f21121078ee29183fe9b5538504ba749
jaacobbbb20 <jhathaway.1@icloud.com>
2025-04-28 15:33:02 -0700

    Finished the frontend setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,23 +2,45 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-// GET route that restores the CSRF token
+// Restores the CSRF Token
 router.get("/api/csrf/restore", (req, res) => {
-
   const csrfToken = req.csrfToken();
-
   res.cookie("XSRF-TOKEN", csrfToken);
-
   res.status(200).json({
     'XSRF-Token': csrfToken
   });
 });
 
+// Mounts the API Router
 router.use('/api', apiRouter);
+
+// Static routes (Production)
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+  router.use(express.static(path.resolve("../frontend/dist")));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+}
+
+
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+
 
 module.exports = router;
