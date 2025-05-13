48aaa562d630f566e00b5699894a011080476db3
jaacobbbb20 <jhathaway.1@icloud.com>
2025-05-13 10:34:34 -0700

    Finished with spot details page for now. moving onto spot form

--- frontend/src/components/Spots/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/Spots/SpotDetails/SpotDetails.jsx
@@ -2,11 +2,14 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getSpotById } from '../../../store/spots';
-
+import './SpotDetails.css'
 function SpotDetails() {
   const { spotId } = useParams();
   const dispatch = useDispatch();
   const spot = useSelector(state => state.spots.singleSpot);
+  const handleClick = () => {
+    alert('Feature not yet implemented. Coming soon...')
+  };
 
   useEffect(() => {
     dispatch(getSpotById(spotId));
@@ -24,27 +27,38 @@ if (!spot.name || !spot.description) {
         <h1>{spot.name}</h1>
         <h2>{spot.city}, {spot.state}, {spot.country}</h2>
       </div>
+
       <div className="spot-images">
-      {spot.SpotImages.length > 0 && (
-        <img 
-          src={spot.SpotImages[0].url} 
-          alt={`Image of ${spot.name}`} 
-          style={{ width: '100%', height: 'auto' }} 
-        />
-      )}
+        {spot.SpotImages.length > 0 && (
+          <img 
+            src={spot.SpotImages[0].url} 
+            alt={`Image of ${spot.name}`} 
+            className="spot-main-image"
+          />
+        )}
       </div>
-      <div className="spot-owner">
-        <h1>Hosted by {spot.Owner ? `${spot.Owner.firstName} ${spot.Owner.lastName}` : `Unknown`}</h1>
+      
+      <div className="spot-details">
+        <div className="left-side">
+          <div className="spot-owner">
+            <h1>Hosted by {spot.Owner ? `${spot.Owner.firstName} ${spot.Owner.lastName}` : `Unknown`}</h1>
+          </div>
+          <div className="spot-description">
+            <p>{spot.description}</p>
+          </div>
+        </div>
+
+        <div className="reserve-button-container">
+          <p>${spot.price} / night</p>
+          <button onClick={handleClick}>Reserve</button>
+        </div>
       </div>
-      <div className="spot-description">
-          <p>{spot.description}</p>
-      </div>
+
+      <hr />
 
       
-      <p>${spot.price} / night</p>
     </div>
   )
-
 }
 
 export default SpotDetails;
